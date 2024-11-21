676ca34ff80aa8e35d0c89344c4907bf393c16ef
Ricardo Souza <ricardoalmeidasouza96@gmail.com>
2024-11-21 19:48:29 -0300

    Refatora o código. Divide as responsabilidades da aplicação

--- server.js
+++ server.js
@@ -1,47 +1,9 @@
 import express from "express";
-
-const posts = [
-    {
-        id: 1,
-        descricao: "Uma foto teste.",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: 2,
-        descricao: "Paisagem incrível!",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: 3,
-        descricao: "Comida deliciosa!",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: 4,
-        descricao: "Um pôr do sol mágico na praia.",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-    {
-        id: 5,
-        descricao: "Cachorro brincando no parque.",
-        imagem: "https://placecats.com/millie/300/150"
-    },
-];
+import routes from "./src/routes/postsRoutes.js";
 
 const app  = express();
-app.use(express.json());
+routes(app);
 
 app.listen(3000, () => {
     console.log("Servidor escutando...");
-});
-
-function buscarPostPorId(id) {
-    return posts.findIndex((post) => {
-        return post.id === Number(id);
-    });
-}
-
-app.get("/posts/:id", (req, res) => {
-    const index = buscarPostPorId(req.params.id);
-    res.status(200).json(posts[index]);
 });
